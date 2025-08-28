5421f8795594a4620dc3e45509bfa1079ba14974
Diego Vazquez Flores <dener22104@gmail.com>
2025-08-28 13:33:52 -0600

    ft: Se genera la estructura de carpetas para la arquitectura limpia
    Se corrigen issues para la funcionalidad de auth
    Cambios en el commit:
    	modified:   src/app.module.ts
    	modified:   src/auth/guards/jwt-auth.guard.ts
    	renamed:    src/auth/decorators/public.decorator.ts -> src/core/decorators/public.decorator.ts
    	new file:   src/core/guards/jwt-auth.guard.ts
    	renamed:    src/auth/guards/local-auth.guard.ts -> src/core/guards/local-auth.guard.ts
    	renamed:    src/prisma.service.ts -> src/infrastructure/prisma.service.ts
    	modified:   src/main.ts
    	renamed:    src/auth/auth.controller.spec.ts -> src/modules/auth/auth.controller.spec.ts
    	renamed:    src/auth/auth.controller.ts -> src/modules/auth/auth.controller.ts
    	renamed:    src/auth/auth.module.ts -> src/modules/auth/auth.module.ts
    	renamed:    src/auth/auth.service.spec.ts -> src/modules/auth/auth.service.spec.ts
    	renamed:    src/auth/auth.service.ts -> src/modules/auth/auth.service.ts
    	renamed:    src/auth/strategies/jwt.strategy.ts -> src/modules/auth/strategies/jwt.strategy.ts
    	renamed:    src/auth/strategies/local.strategy.ts -> src/modules/auth/strategies/local.strategy.ts
    	renamed:    src/users/users.controller.spec.ts -> src/modules/users/users.controller.spec.ts
    	renamed:    src/users/users.controller.ts -> src/modules/users/users.controller.ts
    	renamed:    src/users/users.module.ts -> src/modules/users/users.module.ts
    	renamed:    src/users/users.service.spec.ts -> src/modules/users/users.service.spec.ts
    	renamed:    src/users/users.service.ts -> src/modules/users/users.service.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { PrismaService } from './prisma.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+
 
 @Module({
   imports: [AuthModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService],
 })
 export class AppModule {}
