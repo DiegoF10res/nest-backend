18636b16a18591ccb733d1cbf736849a1c2cb26e
Diego Vazquez Flores <dener22104@gmail.com>
2025-08-29 06:03:58 -0600

    ft: Se implementa estructura para el modulo de branch Cambios en el commit: 	modified:   prisma/dev.db 	modified:   src/app.module.ts 	new file:   src/modules/branch/branch.controller.spec.ts 	new file:   src/modules/branch/branch.controller.ts 	new file:   src/modules/branch/branch.module.ts 	new file:   src/modules/branch/branch.service.spec.ts 	new file:   src/modules/branch/branch.service.ts 	new file:   src/modules/branch/dto/create-branch.dto.ts 	new file:   src/modules/branch/dto/update-branch.dto.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { ProductModule } from './modules/product/product.module';
 import { ProviderModule } from './modules/provider/provider.module';
+import { BranchModule } from './modules/branch/branch.module';
 
 
 @Module({
-  imports: [AuthModule, UsersModule, ProductModule, ProviderModule],
+  imports: [AuthModule, UsersModule, ProductModule, ProviderModule, BranchModule],
   controllers: [AppController],
   providers: [AppService],
 })
