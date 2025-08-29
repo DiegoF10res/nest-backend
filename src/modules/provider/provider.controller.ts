51dc6dd50e221f35d38fc2f7c80adcd66e9e637f
Diego Vazquez Flores <dener22104@gmail.com>
2025-08-29 05:34:58 -0600

    ft: Se implementa servicio para crear un provider Cambios en el commit: 	modified:   src/modules/provider/provider.controller.ts 	modified:   src/modules/provider/provider.module.ts 	modified:   src/modules/provider/provider.service.ts

--- src/modules/provider/provider.controller.ts
+++ src/modules/provider/provider.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ProviderService } from './provider.service';
+import { CreateProviderDto } from './dto/create-provider.dto';
 
 @Controller('provider')
-export class ProviderController {}
+export class ProviderController {
+
+    constructor(private readonly providerService: ProviderService) {}
+
+    @Post()
+    async createProvider(@Body() createProviderDto: CreateProviderDto) {
+        return this.providerService.create(createProviderDto);
+    }
+
+}
--- src/modules/provider/provider.service.ts
+++ src/modules/provider/provider.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/infrastructure/prisma.service';
 
 @Injectable()
-export class ProviderService {}
+export class ProviderService {
+    constructor(private readonly prisma: PrismaService) { }
+
+    async create(CreateProviderDto) {
+        return this.prisma.provider.create({ data: CreateProviderDto });
+    }
+
+}
