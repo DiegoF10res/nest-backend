5aaf53cc1548e17f05ae15b8a53d3ea3f3c658d4
Diego Vazquez Flores <dener22104@gmail.com>
2025-08-29 06:08:36 -0600

    ft: Se implementa servicio para crear branches Cambios en el commit: 	modified:   src/modules/branch/branch.controller.ts 	modified:   src/modules/branch/branch.module.ts 	modified:   src/modules/branch/branch.service.ts

--- src/modules/branch/branch.controller.ts
+++ src/modules/branch/branch.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { BranchService } from './branch.service';
+import { CreateBranchDto } from './dto/create-branch.dto';
 
 @Controller('branch')
-export class BranchController {}
+export class BranchController {
+
+    constructor(private readonly branchService: BranchService) {}
+
+    @Post()
+    async createBranch(@Body() createBranchDto: CreateBranchDto) {
+        return this.branchService.create(createBranchDto);
+    }
+
+}
--- src/modules/branch/branch.service.ts
+++ src/modules/branch/branch.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/infrastructure/prisma.service';
+import { CreateBranchDto } from './dto/create-branch.dto';
 
 @Injectable()
-export class BranchService {}
+export class BranchService {
+
+    constructor(private readonly prisma: PrismaService) { }
+
+    async create(createBranchDto: CreateBranchDto) {
+        return this.prisma.branch.create({ 
+            data: createBranchDto 
+        });
+    }
+
+}
